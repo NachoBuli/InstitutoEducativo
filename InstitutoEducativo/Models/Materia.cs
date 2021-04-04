814ceb4e9a8610069f1e1458fb13cba331bb82f1
timaenho <tom.maenhout@gmail.com>
2021-04-03 21:34:49 -0300

    Anadir propiedades de Materia y Usuario

--- InstitutoEducativo/Models/Materia.cs
+++ InstitutoEducativo/Models/Materia.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 public class Materia
 {
 	public Materia()
 	{
-	}
+	public Guid MateriaId { get; set; }
+
+	public string CodigoMateria { get; set; }
+
+	public string Nombre { get; set; }
+
+	public string Descripcion { get; set; }
+
+	public int CupoMaximo { get; set; }
+
+	public ICollection <MateriaCursada> MateriasCursadas { get; set; }
+
+	public ICollection <Calificacion> Calificaciones { get; set; }
+
+	public Carrera carrera { get; set; }
+
+}
 }
--- InstitutoEducativo/Models/Usuario.cs
+++ InstitutoEducativo/Models/Usuario.cs
@@ -4,6 +4,27 @@ public abstract class Usuario
 {
 	public  Usuario()
 	{
-		public string nombre { get; set; }
-	}
+		public Guid UsuarioId { get; set; }
+
+		public string NombreUsu { get; set; }
+		
+		public string Nombre { get; set; }
+		
+		public string Apellido { get; set; }
+
+		public string Email { get; set; }
+
+		public DateTime FechaAlta { get; set; }
+
+		public string Password { get; set; }
+
+		public string Telefono { get; set; }
+
+		public string Direccion { get; set; }
+
+		public string Dni { get; set; }
+
+}
+
+
 }
