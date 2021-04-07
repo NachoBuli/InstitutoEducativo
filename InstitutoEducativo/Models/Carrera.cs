e980d04ad01c200db83b6e0b2935078644aeacab
Ignacio Bulian <43737055@comunidadort.edu.ar>
2021-04-07 19:11:30 -0300

    Creacion de atributos de la clase Carrera y Profesor

--- InstitutoEducativo/Models/Carrera.cs
+++ InstitutoEducativo/Models/Carrera.cs
@@ -2,6 +2,15 @@
 
 public class Carrera
 {
+	public Guid CarreraId { get; set; }
+
+	public string Nombre { get; set; }
+
+	public ICollection <Materia> Materias { get; set; }
+
+	public ICollection <Alumno> Alumnos { get; set; }
+
+
 	public Carrera()
 	{
 	}
--- InstitutoEducativo/Models/Profesor.cs
+++ InstitutoEducativo/Models/Profesor.cs
@@ -2,6 +2,12 @@
 
 public class Profesor : Usuario
 {
+	public string Legajo { get; set; }
+
+	public ICollection <MateriaCursada> MateriasCursadasActivas { get; set; }
+	
+	public ICollection <Calificacion> CalificacionesRealizadas { get; set; }
+
 	public Profesor()
 	{
 
