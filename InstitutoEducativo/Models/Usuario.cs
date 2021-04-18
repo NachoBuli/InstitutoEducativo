9742f8e27bf2d735e52b23f27899ac961e5c4e60
Tom Maenhout <tom.maenhout@gmail.com>
2021-04-18 13:08:16 -0300

    Agregacion namespace Usuario

--- InstitutoEducativo/Models/Usuario.cs
+++ InstitutoEducativo/Models/Usuario.cs
@@ -4,25 +4,29 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class Usuario
+namespace InstitutoEducativo.Models
 {
-	[Key]
-	public Guid UsuarioId { get; set; }
+	public class Usuario
+	{
+		[Key]
+		public Guid UsuarioId { get; set; }
 
-	[Required (ErrorMessage = Validaciones._required)]
-	[MaxLength (ErrorMessage = Validaciones._maxLength)]
-	public string Nombre { get; set; }
+		[Required(ErrorMessage = Validaciones._required)]
+		[MaxLength(ErrorMessage = Validaciones._maxLength)]
+		public string Nombre { get; set; }
 
-	[DataType(DataType.Date)]
-	[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-	public DateTime FechaAlta { get; set; }
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime FechaAlta { get; set; }
 
-	[Required(ErrorMessage = Validaciones._required)]
-	[DataType(DataType.Password)]
-	public string Password { get; set; }
+		[Required(ErrorMessage = Validaciones._required)]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
 
-	[ForeignKey(nameof(Persona))]
-	public Persona Persona { get; set; }
+		[ForeignKey(nameof(Persona))]
+		public Persona Persona { get; set; }
 
+
+	}
 
 }
