0a7aa61e4aa74e71e6706f9fc58d4fc8016cf6fb
Ignacio Bulian <43737055@comunidadort.edu.ar>
2021-04-28 12:39:35 -0300

    DataAnnotations en Calificacion y Carrera

--- InstitutoEducativo/Models/Calificacion.cs
+++ InstitutoEducativo/Models/Calificacion.cs
@@ -1,5 +1,9 @@
-﻿
+﻿using InstitutoEducativo.Data;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace InstitutoEducativo.Models
 {
@@ -7,14 +11,18 @@ namespace InstitutoEducativo.Models
 	{
 		public Guid CalificacionId { get; set; }
 
+		[Required(ErrorMessage = Validaciones._required)]
 		public int NotaFinal { get; set; }
 
+		[Required(ErrorMessage = Validaciones._required)]
 		public Materia Materia { get; set; }
 
-        public MateriaCursada MateriaCursada { get; set; }
+		[Required(ErrorMessage = Validaciones._required)]
+		public MateriaCursada MateriaCursada { get; set; }
 
 		public Profesor Profesor { get; set; }
 
+		[Required(ErrorMessage = Validaciones._required)]
 		public Alumno Alumno { get; set; }
 
 	
--- InstitutoEducativo/Models/Carrera.cs
+++ InstitutoEducativo/Models/Carrera.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using InstitutoEducativo.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,15 +12,12 @@ namespace InstitutoEducativo.Models
 	{
 		public Guid CarreraId { get; set; }
 
+		[Required(ErrorMessage = Validaciones._required)]
+		[MaxLength(50, ErrorMessage = Validaciones._maxLength)]
 		public string Nombre { get; set; }
 
 		public ICollection<Materia> Materias { get; set; }
 
 		public ICollection<Alumno> Alumnos { get; set; }
-
-
-		public Carrera()
-		{
-		}
 	}
 }
